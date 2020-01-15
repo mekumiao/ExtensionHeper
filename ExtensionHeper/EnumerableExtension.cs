f2059d03bbae4800d746a64cc92b6d02f9a30eff
qiqigou <2319002545@qq.com>
2020-01-15 15:23:59 +0800

    添加项目文件。

---
+++ ExtensionHeper/EnumerableExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// 枚举器、集合 遍历扩展
+    /// </summary>
+    public static class EnumerableExtension
+    {
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            foreach (var item in source)
+            {
+                action.Invoke(item);
+            }
+        }
+
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource, int> action)
+        {
+            var index = 0;
+            foreach (var item in source)
+            {
+                action.Invoke(item, index);
+                ++index;
+            }
+        }
+
+        public static void ForEach<TSource>(this List<TSource> source, Action<TSource, int> action)
+        {
+            var index = 0;
+            foreach (var item in source)
+            {
+                action.Invoke(item, index);
+                ++index;
+            }
+        }
+    }
+}
---
+++ ExtensionHeper/push.bat
@@ -0,0 +1,8 @@
+
+devenv "..\ExtensionHeper.sln" /build Release /project "ExtensionHeper.csproj"
+
+nuget pack ExtensionHeper.csproj -Version 1.0.0.2
+
+nuget push .\*.nupkg -Source http://onlyoned.com:9081 -ApiKey oy2jfy4h2v4cw3oxb7qqya7dr2vhyd3ulbahapgvaj2ycm
+
+pause
