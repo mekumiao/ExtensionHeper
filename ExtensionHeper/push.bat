30c4f895a1719896a792bece704bf2183fbc385e
qiqigou <2319002545@qq.com>
2020-01-16 13:28:10 +0800

    1

--- ExtensionHeper/push.bat
+++ ExtensionHeper/push.bat
@@ -1,7 +1,7 @@
 
 devenv "..\ExtensionHeper.sln" /build Release /project "ExtensionHeper.csproj"
 
-nuget pack ExtensionHeper.csproj -Version 1.0.0.2
+nuget pack ExtensionHeper.csproj -Version 1.0.0.3
 
 nuget push .\*.nupkg -Source http://onlyoned.com:9081 -ApiKey oy2jfy4h2v4cw3oxb7qqya7dr2vhyd3ulbahapgvaj2ycm
 
